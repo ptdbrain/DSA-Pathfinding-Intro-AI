5e39bc3dde063ad1d63d8e6b03c96cdabec92f00
Brain <dtp0382@gmail.com>
2025-05-08 21:01:21 +0700

    update ui

--- app.py
+++ app.py
@@ -5,166 +5,219 @@ from algorithm.a_star import astar
 from algorithm.dijkstra import dijkstra
 from algorithm.bfs import bfs
 from algorithm.dfs import dfs
+from algorithm.greedy_best_first import greedy_best_first
+from algorithm.iterative_deepening_dfs import iddfs
+from algorithm.uniform_cost_search import uniform_cost_search
 
-
-edges_file = 'data/fileCsv/adj_list_with_weights.csv'   #đường dẫn đến file csv chứa danh sách cạnh
+edges_file = 'data/fileCsv/adj_list_with_weights.csv'
 adj_dict = {}
 
-with open(edges_file, 'r') as f:        # Mở file CSV
+with open(edges_file, 'r') as f:
     reader = csv.reader(f)
-    header = next(reader)   # Bỏ qua dòng đầu tiên (tiêu đề)
+    header = next(reader)
     for row in reader:
         try:
-            node = int(row[0])  # Chuyển đổi node thành số nguyên
+            node = int(row[0])
             neighbors = {}
 
             raw_text = row[1]
 
             # In vài dòng đầu để kiểm tra
             if node < 5:
-                print(f"Node {node}, raw text: {raw_text}")     # In ra 5 dòng đầu tiên để kiểm tra
+                print(f"Node {node}, raw text: {raw_text}")
 
             try:
-                parsed_data = eval(raw_text.replace('np.float64', ''))  # Chuyển đổi chuỗi thành tuple
-                for neighbor_id, weight in parsed_data: 
-                    neighbors[int(neighbor_id)] = float(weight) # Chuyển đổi id và trọng số thành số nguyên và số thực
-            except:     # Nếu eval không thành công, xử lý theo cách khác
+                parsed_data = eval(raw_text.replace('np.float64', ''))
+                for neighbor_id, weight in parsed_data:
+                    neighbors[int(neighbor_id)] = float(weight)
+            except:
                 raw = raw_text.replace('[', '').replace(']', '').replace('np.float64', '') \
-                              .replace('(', '').replace(')', '')    # Xóa các ký tự không cần thiết
-                parts = raw.split(',')  # Tách chuỗi thành danh sách các phần tử
+                                .replace('(', '').replace(')', '')
+                parts = raw.split(',')
 
-                for i in range(0, len(parts) - 1, 2):   # Duyệt qua các phần tử theo cặp
-                    if parts[i].strip() and parts[i+1].strip(): # Kiểm tra xem phần tử có rỗng không
-                        neighbor_id = int(parts[i].strip()) # Chuyển đổi id thành số nguyên
-                        weight = float(parts[i+1].strip())  # Chuyển đổi trọng số thành số thực
-                        neighbors[neighbor_id] = weight # Thêm vào từ điển neighbors
+                for i in range(0, len(parts) - 1, 2):
+                    if parts[i].strip() and parts[i+1].strip():
+                        neighbor_id = int(parts[i].strip())
+                        weight = float(parts[i+1].strip())
+                        neighbors[neighbor_id] = weight
 
-            adj_dict[node] = neighbors  # Thêm vào từ điển adj_dict
+            adj_dict[node] = neighbors
 
         except Exception as e:
-            print(f"Lỗi khi xử lý node {row[0]}: {e}")  # In lỗi nếu có
+            print(f"Lỗi khi xử lý node {row[0]}: {e}")
             continue
-print(f"Đã đọc được {len(adj_dict)} nodes từ adj_list") # In ra số lượng node đã đọc được
+print(f"Đã đọc được {len(adj_dict)} nodes từ adj_list")
 
 # Flask app
-app = Flask(__name__)   # Tạo ứng dụng Flask
+app = Flask(__name__)
 CORS(app)
 
 adj_list = adj_dict  # Gán đồ thị gốc
 
-@app.route('/find_path', methods=['POST'])  # Định nghĩa route cho API
+@app.route('/find_path', methods=['POST'])
 def find_path():
     data = request.get_json()
 
     # lấy dữ liệu từ request
-    start = int(data['start'])  # Chuyển đổi start thành số nguyên
-    end = int(data['end'])      # Chuyển đổi end thành số nguyên
-    num_iterations = int(data.get('iterations', 10))    # Số lần lặp (mặc định là 10)
-    blocked_edges = data.get('blocked_edges', [])       # Các cạnh bị cấm (mặc định là rỗng)
-    algorithm = data.get('algorithm', 'A*') # Thuật toán (mặc định là A*)
-    trafic_edges = data.get('traffic_edges', [])    # Các cạnh tắc (mặc định là rỗng)
-    trafic_level = data.get('traffic_level', 0) # Mức độ tắc (mặc định là 0)
+    start = int(data['start'])
+    end = int(data['end'])
+    num_iterations = int(data.get('iterations', 10))
+    blocked_edges = data.get('blocked_edges', [])
+    algorithm = data.get('algorithm', 'A*')
+    trafic_edges = data.get('traffic_edges', [])
+    trafic_level = data.get('traffic_level', 0)
+    flood_edges = data.get('flood_edges', [])
+    flood_level = data.get('flood_level', 0)
+    one_way_edges = data.get('one_way_edges', [])
 
-     # kiểm tra điều kiện đầu vào 
-    if 'start' not in data or 'end' not in data:    # Kiểm tra xem có 'start' và 'end' trong dữ liệu không
-        return jsonify({"error": "Missing 'start' or 'end' node in request data"}), 400     # Nếu không có thì trả về lỗi 400
-    if start not in adj_list or end not in adj_list:    # Kiểm tra xem 'start' và 'end' có trong đồ thị không
-        return jsonify({"error": f"Invalid 'start' or 'end' node. {start} or {end} not found in the graph."}), 400   # Nếu không có thì trả về lỗi 400
+    # kiểm tra điều kiện đầu vào 
+    if 'start' not in data or 'end' not in data:
+        return jsonify({"error": "Missing 'start' or 'end' node in request data"}), 400
+    if start not in adj_list or end not in adj_list:
+        return jsonify({"error": f"Invalid 'start' or 'end' node. {start} or {end} not found in the graph."}), 400
     
     
     # Kiểm tra nếu start hoặc end nằm trong các cạnh bị cấm
     for edge in blocked_edges:
         if len(edge) != 2:
             continue
-        u, v = edge # Lấy các đỉnh của cạnh bị cấm
+        u, v = edge
         # Kiểm tra nếu start hoặc end là một phần của cạnh bị cấm
-        if start == u and end == v or start == v and end == u:  # Nếu start và end nằm trong cạnh bị cấm
-            return jsonify({"error": f"💀Vị trí bạn chọn nằm trong vùng cấm!!!\nVui lòng chọn lại vị trí"}), 400        
+        if start == u and end == v or start == v and end == u:
+            return jsonify({"error": f"💀Vị trí bạn chọn nằm trong vùng cấm!!!\nVui lòng chọn lại vị trí"}), 400
 
 
 
     print(f"Received data: {data}")
     # Kiểm tra dữ liệu đầu vào 
-    print(f"\n=== Finding path from {start} to {end} ===")      
+    print(f"\n=== Finding path from {start} to {end} ===")
     print(f"Algorithm: {algorithm}")
     print(f"Number Blocked edges: {len(blocked_edges)}")
     print(f"Number Traffic edges: {len(trafic_edges)}")
-    print(f"Traffic level: {trafic_level}") 
+    print(f"Traffic level: {trafic_level}")
+    print(f"Number Flood edges: {len(flood_edges)}")
+    print(f"Flood level: {flood_level}")
     print(f"Number of iterations: {num_iterations}")
-
-
-   
     try:
         # Tạo bản sao đồ thị và xóa các cạnh bị cấm
-        from copy import deepcopy   # Sử dụng deepcopy để tạo bản sao sâu của đồ thị
-        adj_list_filtered = deepcopy(adj_list)  # Tạo bản sao của đồ thị gốc để không làm thay đổi nó
+        from copy import deepcopy
+        adj_list_filtered = deepcopy(adj_list)
+        
+        for one_way_edge in one_way_edges:
+            if len(one_way_edge) == 2:
+                source, destination = one_way_edge
+                # Kiểm tra xem destination có trong đồ thị và source có phải là hàng xóm của destination không
+                if destination in adj_list_filtered and isinstance(adj_list_filtered[destination], dict):
+                    adj_list_filtered[destination].pop(source, None) # Xóa source khỏi danh sách kề của destination
 
         #------------------------------- Xử lý các cạnh bị cấm --------------------------------#     
-        for edge in blocked_edges:      
-            if len(edge) != 2:  ## Kiểm tra xem cạnh có đúng định dạng không
+        for edge in blocked_edges:
+            if len(edge) != 2:
                 continue
             u, v = edge
             
             # Xóa cạnh theo cả hai chiều
-            if isinstance(adj_list_filtered[u], dict):      ## Nếu là dict thì xóa theo chiều u -> v
-                adj_list_filtered[u].pop(v, None)   
-            elif isinstance(adj_list_filtered[u], list):    ## Nếu là list thì xóa theo chiều u -> v
-                if v in adj_list_filtered[u]:               ## Kiểm tra xem v có trong danh sách không
+            if isinstance(adj_list_filtered[u], dict):
+                adj_list_filtered[u].pop(v, None)
+            elif isinstance(adj_list_filtered[u], list):
+                if v in adj_list_filtered[u]:
                     adj_list_filtered[u].remove(v)
-
-            if isinstance(adj_list_filtered[v], dict):    ## Nếu là dict thì xóa theo chiều v -> u
+                    
+            if isinstance(adj_list_filtered[v], dict):
                 adj_list_filtered[v].pop(u, None)
-            elif isinstance(adj_list_filtered[v], list):    ## Nếu là list thì xóa theo chiều v -> u
+            elif isinstance(adj_list_filtered[v], list):
                 if u in adj_list_filtered[v]:
-                    adj_list_filtered[v].remove(u)          #   Xóa cạnh theo chiều v -> u
-            # Nếu không có cạnh nào thì xóa node
+                    adj_list_filtered[v].remove(u)
 
         #------------------------------- Xử lý các cạnh tắc --------------------------------#
+        if int(trafic_level) == 1:
+            k = 1.2
+        elif int(trafic_level) == 2:
+            k = 1.5
+        elif int(trafic_level) == 3:
+            k = 2
         for edge in trafic_edges:
-            if len(edge) != 2:  ## Kiểm tra xem cạnh có đúng định dạng không
+            if len(edge) != 2:
                 continue
-            u, v = edge ## Lấy các đỉnh của cạnh tắc
+            u, v = edge
             # Kiểm tra xem cạnh có tồn tại trong đồ thị không
-            if u in adj_list_filtered and v in adj_list_filtered[u]:    ## Nếu có thì cập nhật trọng số
+            if u in adj_list_filtered and v in adj_list_filtered[u]:
                 # Cập nhật trọng số của cạnh
-                adj_list_filtered[u][v] *= int(trafic_level)    ## Cập nhật trọng số của cạnh theo chiều u -> v
-            if v in adj_list_filtered and u in adj_list_filtered[v]:#  Nếu có thì cập nhật trọng số
+                adj_list_filtered[u][v] *= k
+            if v in adj_list_filtered and u in adj_list_filtered[v]:
                 # Cập nhật trọng số của cạnh theo chiều ngược lại
-                adj_list_filtered[v][u] *= int(trafic_level)    ## Cập nhật trọng số của cạnh theo chiều v -> u
+                adj_list_filtered[v][u] *= k
+                
+                
+        #------------------------------- Xử lý các cạnh ngập --------------------------------#
+        flood_level = int(flood_level)
+        if flood_level == 1:
+            f = 1.5
+        elif flood_level == 2:
+            f = 2
+        elif flood_level == 3:
+            f = 99999999
+        for edge in flood_edges:
+            if len(edge) != 2:
+                continue
+            u, v = edge
+            if (f != 99999999):
+            # Kiểm tra xem cạnh có tồn tại trong đồ thị không
+                if u in adj_list_filtered and v in adj_list_filtered[u]:
+                    # Cập nhật trọng số của cạnh
+                    adj_list_filtered[u][v] *= f
+                if v in adj_list_filtered and u in adj_list_filtered[v]:
+                    # Cập nhật trọng số của cạnh theo chiều ngược lại
+                    adj_list_filtered[v][u] *= f
+            else:
+                if isinstance(adj_list_filtered[u], dict):
+                    adj_list_filtered[u].pop(v, None)
+                elif isinstance(adj_list_filtered[u], list):
+                    if v in adj_list_filtered[u]:
+                        adj_list_filtered[u].remove(v)
 
+                if isinstance(adj_list_filtered[v], dict):
+                    adj_list_filtered[v].pop(u, None)
+                elif isinstance(adj_list_filtered[v], list):
+                    if u in adj_list_filtered[v]:
+                        adj_list_filtered[v].remove(u)
     
         algorithms = {
             'A Star': astar,
             'Dijkstra': dijkstra,
             'BFS': bfs,
-            'DFS': dfs
+            'DFS': dfs,
+            'Greedy Best First' : greedy_best_first,
+            'Iterative Deepening DFS' : iddfs,
+            'Uniform Cost Search' : uniform_cost_search,
         }
 
         if algorithm in algorithms:
-            path, explored_nodes = algorithms[algorithm](adj_list_filtered, start, end, num_iterations) # Gọi hàm tìm đường đi theo thuật toán đã chọn
-            list_explore_node = list(explored_nodes)    # Chuyển đổi tập hợp thành danh sách để dễ dàng in ra
-            print("\n✅List of explored nodes:")             
+            path, explored_nodes = algorithms[algorithm](adj_list_filtered, start, end, num_iterations)
+            list_explore_node = list(explored_nodes)
+            print("\n✅List of explored nodes:")
             print(list_explore_node)
 
             if path:
                 print("\n✅ Path found:", path)
-                list_explore_node.insert(0, start)           # Thêm node bắt đầu vào danh sách đã khám phá
-                list_explore_node.append(end)             # Thêm node kết thúc vào danh sách đã khám phá
+                list_explore_node.insert(0, start)
+                list_explore_node.append(end)
                 return jsonify({
-                    "path": path,       ## Trả về đường đi
-                    "explored_nodes": list_explore_node,    ## Trả về danh sách các node đã khám phá  
-                    "message": "Path found successfully." ## Trả về thông báo thành công
+                    "path": path,
+                    "explored_nodes": list_explore_node,
+                    "message": "Path found successfully.",
+                    "Cost": "someone"
                 })
             else:
                 print("❌ No path found.")
-                return jsonify({"error": "No path found between the nodes."}), 404      # Nếu không tìm thấy đường đi thì trả về lỗi 404
+                return jsonify({"error": "No path found between the nodes."}), 404
         else:
-            return jsonify({"error": "Invalid algorithm specified."}), 400  # Nếu thuật toán không hợp lệ thì trả về lỗi 400
+            return jsonify({"error": "Invalid algorithm specified."}), 400
 
     except Exception as e:
-        print(f"❌ Không tìm thấy đường đi")
-        return jsonify({"error": "❌ Không tìm thấy đường đi"}), 500    # Nếu có lỗi xảy ra thì trả về lỗi 500
+        print(f"❌ Có lỗi xảy ra ")
+        return jsonify({"error": "❌ Có lỗi xảy ra "}), 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True) # Chạy ứng dụng Flask với chế độ debug
+    app.run(debug=True)
